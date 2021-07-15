8b9d9d2c0ef7e110ecf0ae993efed578fa165b9d
tkachukione <tkachukione@gmail.com>
2021-07-15 19:43:38 +0300

    Updates

---
+++ precache-manifest.321545cd9894f95b1b78f57a09f89f1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "504f6caabce0720c368a3d7e0b787038",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9ec4d2d1486276ec7b27",
+    "url": "./static/css/main.6338940e.chunk.css"
+  },
+  {
+    "revision": "a3fa78d2a47eeb043805",
+    "url": "./static/js/2.ac9e10eb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.ac9e10eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ec4d2d1486276ec7b27",
+    "url": "./static/js/main.3d0601e2.chunk.js"
+  },
+  {
+    "revision": "8b3c3b96386eee1ea33a",
+    "url": "./static/js/runtime-main.b672bc02.js"
+  }
+]);
